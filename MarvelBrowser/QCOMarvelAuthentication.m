50abb93fdf3ab495ce0db8dfa68e615ed30b37eb
Emily Priddy <emily@headstormstudios.com>
2016-06-06 17:55:54 -0500

    Use dot-notation property

--- MarvelBrowser/QCOMarvelAuthentication.m
+++ MarvelBrowser/QCOMarvelAuthentication.m
@@ -12,7 +12,7 @@
 
 + (NSString *)timestamp
 {
-    return @([[NSDate date] timeIntervalSinceReferenceDate]).stringValue;
+    return @([NSDate date].timeIntervalSinceReferenceDate).stringValue;
 }
 
 @end
